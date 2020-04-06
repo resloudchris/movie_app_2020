1f744a1431184705e2dcaacf376ac452a94f893c
Christopher Lee <christopherlee0222@users.noreply.github.com>
2020-04-06 15:30:55 +0800

    Updates

---
+++ precache-manifest.14525f92bcae73fc60b53068728ab695.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "824eaf4e1095fc9dd640d0b5a8f61305",
+    "url": "/movie_app_2020/index.html"
+  },
+  {
+    "revision": "7f7b3905fb40f9e008ee",
+    "url": "/movie_app_2020/static/css/main.196d3b4e.chunk.css"
+  },
+  {
+    "revision": "2731f645873efd1058d0",
+    "url": "/movie_app_2020/static/js/2.a7e02d2d.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/movie_app_2020/static/js/2.a7e02d2d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7f7b3905fb40f9e008ee",
+    "url": "/movie_app_2020/static/js/main.bc9c9481.chunk.js"
+  },
+  {
+    "revision": "8b67d7ccc50a17c9e597",
+    "url": "/movie_app_2020/static/js/runtime-main.aaeb0f22.js"
+  }
+]);
